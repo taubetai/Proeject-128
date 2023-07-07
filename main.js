1fa4df8b46b5c66f6e9076dab58cf960f1231397
taubetai <114882136+taubetai@users.noreply.github.com>
2023-07-07 09:43:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
+function preload()
+{
+    song = loadSound("NGGUUP.mp3")
+}
+
+function setup(){
+    canvas = createCanvas(600,580);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('poses', gotPoses);
+}
+function modelLoaded() {
+    console.log('PoseNetIs Initialized');
+}
+function draw() {
+    image(video,0,0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+
+    
+}
+function gotPoses(results)
+    {
+if(results.length> 0)
+{
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristX = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX + "leftWristY = "+ leftWristY);
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristX = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX + "rightWristY = "+ rightWristY);
+}
+    }
